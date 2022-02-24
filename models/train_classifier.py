ff590afb5bf53d73ebfc19c40edc93cc5325167b
Ajax243 <m.osamasalman@gmail.com>
2022-02-24 21:22:01 +0000

    Initial commit

---
+++ models/train_classifier.py
@@ -0,0 +1,105 @@
+import sys
+# import libraries
+import numpy as np
+import pandas as pd
+import sqlite3
+import re
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sqlalchemy import create_engine
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+import pickle
+
+
+def load_data(database_filepath):
+    database_file='sqlite:///'+ database_filepath
+    engine = create_engine(database_file)
+   
+    df = pd.read_sql('SELECT * FROM disaster_table', engine)
+
+    x = df.message
+    Y = df.iloc[:,2:]
+    category_names=Y.columns
+    return x,Y, category_names
+
+
+def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text.replace(url,'url_placeholder')
+    tokens=word_tokenize(text)
+    lemmatizer=WordNetLemmatizer()
+    
+    clean_tokens=[]
+    for token in tokens:
+        clean_tokens.append(lemmatizer.lemmatize(token).lower().strip())
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('moc' , MultiOutputClassifier(RandomForestClassifier(n_estimators=50,max_leaf_nodes=100,max_depth=3, warm_start=False)))])
+    x,Y,category_names=load_data('DisasterResponse.db')   
+    x_train,x_test,Y_train,Y_test=train_test_split(x,Y,test_size=0.20, random_state=42)
+    pipeline.fit(x_train,Y_train)
+    
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred=pd.DataFrame(model.predict(X_test))
+    Y_test=pd.DataFrame(Y_test)
+    
+    
+    for col in range(len(category_names)):
+        print(category_names[col])
+        print (classification_report(Y_test.iloc[:,col], Y_pred.iloc[:,col]))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+    
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
